22c067f373c5b978be63988172995f7ac876c8fd
Luis Guilherme Pelinson Giani <lggiani@gmail.com>
2025-10-12 20:41:38 -0300

    Update front.py

--- Python/front.py
+++ Python/front.py
@@ -1,8 +1,9 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.window.width = 430
+    page.window.height = 620
     texto = ft.Text(value="Hello, world!", color="green")
-    page.controls.append(texto)
-    page.update()
+    page.add(texto)
 
 ft.app(main)
