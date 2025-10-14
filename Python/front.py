c5f54d73f8ad58797b5a5ffbf05af18d29024d36
Luis Guilherme Pelinson Giani <lggiani@gmail.com>
2025-10-13 21:03:46 -0300

    Update front.py

--- Python/front.py
+++ Python/front.py
@@ -4,6 +4,12 @@ def main(page: ft.Page):
     page.window.width = 430
     page.window.height = 620
     texto = ft.Text(value="Hello, world!", color="green")
-    page.add(texto)
+    page.add(
+	ft.Row(controls=[
+		ft.Text(texto.value),
+		ft.Text("B"),
+		ft.Text("C")
+		])
+	)
 
 ft.app(main)
