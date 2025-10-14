ac2b1074ab0c2a658453eab9967ebe98eb286beb
Luis Guilherme Pelinson Giani <lggiani@gmail.com>
2025-10-13 21:19:03 -0300

    Update front.py

--- Python/front.py
+++ Python/front.py
@@ -3,13 +3,20 @@ import flet as ft
 def main(page: ft.Page):
     page.window.width = 430
     page.window.height = 620
+    page.bgcolor = "gray"
     texto = ft.Text(value="Hello, world!", color="green")
     page.add(
 	ft.Row(controls=[
-		ft.Text(texto.value),
+		texto,
 		ft.Text("B"),
 		ft.Text("C")
 		])
 	)
+    page.add(
+    ft.Row(controls=[
+        ft.TextField(label="Digite aqui:"),
+        ft.ElevatedButton(text="Clique aqui!")
+    ])
+    )
 
 ft.app(main)
