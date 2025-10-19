cca5e4b15e685a4f83fb75b4e65d89939e5d2e1c
Luis Guilherme Pelinson Giani <lggiani@gmail.com>
2025-10-19 18:02:07 -0300

    Update front.py

--- Python/front.py
+++ Python/front.py
@@ -1,26 +1,23 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     page.title = "Projetinho teste"
     page.window.width = 830
     page.window.height = 800
     page.bgcolor = "gray"
+    page.spacing = 20
+    page.vertical_alignment = 'center'
+    page.horizontal_alignment = 'center'
 
     titulo = ft.Text(
-        value=""
+        value="Teste",
+        size= 32
     )
-    def valida_resposta(digitar):
-        if digitar.value == '1':
-            ft.Text(value="certo")
-        else:
-            ft.Text(value='Errado')
-
-        page.update()
 
     texto =  ft.Text(value="Qual é a cor de plano de fundo?", 
                 color="white", 
                 size= 20, 
-                text_align='center'
                 )
 
     digitar = ft.TextField(label="Digite aqui a sua resposta:", 
@@ -30,9 +27,9 @@ def main(page: ft.Page):
     
     btn = ft.ElevatedButton(
             text="Responder", 
-            on_click= valida_resposta(digitar)
+           # on_click= valida_resposta(digitar)
             )
 
-    page.add(texto, digitar, btn)
+    page.add(titulo, texto, digitar, btn)
 
 ft.app(main)
